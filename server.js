cd3ac0885cb16fdbf66845c6dd0c38a17765acf1
Damir Kolobaric <dkolobaric@gmail.com>
2022-11-06 01:05:56 +0100

    Style

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
+//Requiring all the necessary files and libraries
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-
 import userRoutes from './routes/userRoute.js'
 
-//Configure dotenv files above using any other library and files
+// Configure dotenv files above using any other library and files
 dotenv.config()
-//Creating an app from express
+// Creating an app from express
 connectDB()
-
 const app = express()
 
-//Using express.json to get request of json data
+// Using express.json to get request of json data
 app.use(express.json())
 
-//Using routes
+// Using routes
 app.use('/api/users', userRoutes)
 
-//listening to the server
+// Listening to the server
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening at ${process.env.PORT}`)
 })
